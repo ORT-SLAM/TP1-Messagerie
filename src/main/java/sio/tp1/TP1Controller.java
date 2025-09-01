03c5d63e1c882959d6ba90fcb3ee352dc4ed130a
CapelleGab <gabin.capelle.pro@gmail.com>
2025-09-01 12:16:20 +0200

    feat : see treeview message

--- src/main/java/sio/tp1/TP1Controller.java
+++ src/main/java/sio/tp1/TP1Controller.java
@@ -91,8 +91,28 @@ public class TP1Controller implements Initializable {
 
     @FXML
     public void cboDestinatairesClicked(Event event) {
+        TreeItem root = new TreeItem("Tous les messages");
+        String selectedDestinataire = cboDestinataires.getSelectionModel().getSelectedItem().toString();
+        if (maMessagerie.containsKey(selectedDestinataire)) {
 
+        maMessagerie.get(selectedDestinataire).forEach(message -> {
+            int i = maMessagerie.get(selectedDestinataire).indexOf(message) + 1;
+            TreeItem number = new TreeItem("Message n°"+ i);
+            TreeItem expediteur = new TreeItem("De ==> " + message.getExpediteur());
+            TreeItem contenu = new TreeItem("Message ==> " + message.getContenuDuMessage());
 
+            root.getChildren().add(number);
+            number.getChildren().add(expediteur);
+            number.getChildren().add(contenu);
+            number.setExpanded(true);
+
+        });
+        } else {
+            TreeItem emptyMessage = new TreeItem("Aucun message");
+            root.getChildren().add(emptyMessage);
+        }
+        tvMessages.setRoot(root);
+        root.setExpanded(true);
 
 
     }
